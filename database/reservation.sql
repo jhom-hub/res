3ec06a9d5b7f89609b6fd42b2fc4d06e1f7cd8ce
Jhomell <jpjaspio8102@gmail.com>
2024-07-21 02:46:17 +0800

    dbCommit

--- database/reservation.sql
+++ database/reservation.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 19, 2024 at 03:40 PM
+-- Generation Time: Jul 20, 2024 at 08:43 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,27 +24,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `calendar`
+-- Table structure for table `appointment`
 --
 
-CREATE TABLE `calendar` (
+CREATE TABLE `appointment` (
   `id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `start_date` date NOT NULL,
-  `end_date` date NOT NULL,
-  `start_time` time NOT NULL,
-  `end_time` time NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `division` varchar(255) NOT NULL,
+  `section` varchar(255) NOT NULL,
+  `concern` varchar(255) NOT NULL,
+  `date` date DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`id`, `full_name`, `division`, `section`, `concern`, `date`, `created_at`) VALUES
+(1, 'SAMPLE', 'SAMPLE', 'SAMPLE', 'WINDOWS UPDATE', NULL, '2024-07-20 14:45:19'),
+(2, 'TEST', 'TEST', 'TEST', 'WIFI CONNECTION', NULL, '2024-07-20 15:10:55'),
+(3, 'AS', 'AS', 'AS', 'WIFI CONNECTION', NULL, '2024-07-20 15:14:48'),
+(4, 'AA', 'A', 'A', 'WINDOWS UPDATE', '2024-07-31', '2024-07-20 15:44:20'),
+(5, 'J', 'J', 'J', 'WIFI CONNECTION', '2024-07-26', '2024-07-20 15:52:40'),
+(6, 'QQ', 'QQ', 'QQ', 'WINDOWS UPDATE', '2024-07-31', '2024-07-20 15:53:35'),
+(7, 'AA', 'AA', 'A', 'WIFI CONNECTION', '2024-07-31', '2024-07-20 17:30:28'),
+(8, 'A', 'A', 'A', 'WINDOWS UPDATE', '2024-07-31', '2024-07-20 17:30:51'),
+(9, 'S', 'S', 'S', 'WIFI CONNECTION', '2024-07-31', '2024-07-20 17:31:09');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `calendar`
+-- Indexes for table `appointment`
 --
-ALTER TABLE `calendar`
+ALTER TABLE `appointment`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -52,10 +67,10 @@ ALTER TABLE `calendar`
 --
 
 --
--- AUTO_INCREMENT for table `calendar`
+-- AUTO_INCREMENT for table `appointment`
 --
-ALTER TABLE `calendar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `appointment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
